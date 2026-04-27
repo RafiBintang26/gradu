752f6faccfb83228ee5ddba3a91011f3bd541156
RafiBintang26 <108195524+RafiBintang26@users.noreply.github.com>
2026-04-27 15:42:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToNext() {
+    // Memberikan efek visual sederhana sebelum pindah
+    console.log("Mengarahkan ke halaman selanjutnya...");
+    
+    // Ganti URL di bawah dengan alamat web tujuanmu
+    window.location.href = "https://t.me"; 
+}
